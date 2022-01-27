62a459705f4cd7b55473c2b45283d2b7de6594df
Jose Rodriguez <jose123135@gmail.com>
2022-01-27 20:48:48 +0100

    Created new views

--- controllers/consult.php
+++ controllers/consult.php
@@ -1,5 +1,5 @@
 <?php
-class consult extends controller {
+class Consult extends controller {
 
 function __construct()
 {
---
+++ views/consult/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Main</title>
+  <meta name="description" content="A simple HTML5 Template for new projects.">
+  <meta name="author" content="SitePoint">
+</head>
+<body>
+<?php require "views/header.php";
+?>
+
+<div id="main">
+<h1 class="center">Consultation yone</h1>
+</div>
+
+<?php require "views/footer.php";
+?>
+</body>
+</html>
